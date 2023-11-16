1597660936716b7b757dfb4174a022e98525332b
MohamedAlaga <malaga.142014@gmail.com>
2023-11-17 00:56:04 +0200

    alaga part 2

---
+++ error_type2.c
@@ -0,0 +1,33 @@
+#include"monty.h";
+void error_type2(int num, ...)
+{
+	va_list ag;
+	char *op;
+	int l_num;
+
+	va_start(ag, num);
+	switch (num)
+	{
+		case 6:
+			fprintf(stderr, "L%d: can't pint, stack empty\n",
+				va_arg(ag, int));
+			break;
+		case 7:
+			fprintf(stderr, "L%d: can't pop an empty stack\n",
+				va_arg(ag, int));
+			break;
+		case 8:
+			l_num = va_arg(ag, unsigned int);
+			op = va_arg(ag, char *);
+			fprintf(stderr, "L%d: can't %s, stack too short\n", l_num, op);
+			break;
+		case 9:
+			fprintf(stderr, "L%d: division by zero\n",
+				va_arg(ag, unsigned int));
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
---
+++ pop_first.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+void pop_first(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL)
+		more_err(7, line_number);
+
+	tmp = *stack;
+	*stack = tmp->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(tmp);
+}
