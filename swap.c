b28cd151ae701069bd7e31581243a576a0271a88
mashaheen <finalflash147@yahoo.com>
2023-11-16 23:54:31 +0200

    swap

---
+++ swap.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * swap - Swaps top two elements of stack.
+ * @stack: Pointer to a pointer pointing to top the stack.
+ * @num: Interger representing the line number opcode.
+ */
+void swap(stack_t **stack, unsigned int num)
+{
+    stack_t *temporary;
+
+    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+        more_err(8, num, "swap");
+    temporary = (*stack)->next;
+    (*stack)->next = temporary->next;
+    if (temporary->next != NULL)
+        temporary->next->prev = *stack;
+    temporary->next = *stack;
+    (*stack)->prev = temporary;
+    temporary->prev = NULL;
+    *stack = temporary;
+}
