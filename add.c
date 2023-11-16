225a422e2290edab4853b8469550a048066e4d2a
mashaheen <finalflash147@yahoo.com>
2023-11-17 00:12:47 +0200

    -add/nop

---
+++ add.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * add - Adds top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @num: Interger representing the line number of of the opcode.
+ */
+void add(stack_t **stack, unsigned int num)
+{
+    int sum;
+
+    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+        more_err(8, num, "add");
+
+    (*stack) = (*stack)->next;
+    sum = (*stack)->n + (*stack)->prev->n;
+    (*stack)->n = sum;
+    free((*stack)->prev);
+    (*stack)->prev = NULL;
+}
---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - Does nothing.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @num: Interger representing the line number of of the opcode.
+ */
+void nop(stack_t **stack, unsigned int num)
+{
+    (void)stack;
+    (void)num;
+}
